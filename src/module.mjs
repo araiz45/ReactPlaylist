fee5492187126436f8a0c0c253de201ad1c17551
Araiz Zahid <araizzahid45@gmail.com>
2023-08-02 03:11:14 +0500

    import export

---
+++ src/module.mjs
@@ -0,0 +1,2 @@
+import ui, {a, c, d} from './module1.mjs';
+console.log(ui, a, c, d);
---
+++ src/module1.mjs
@@ -0,0 +1,9 @@
+const a = 'araiz';
+const b = 'injir';
+const c = 'miswar';
+const d = 'pahto';
+
+export default b;
+export {a};
+export {c};
+export {d};
