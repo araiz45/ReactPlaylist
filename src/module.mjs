0b851fba8b16ec5fb8bdfc733691e8cdf8a2ef7d
Araiz Zahid <araizzahid45@gmail.com>
2023-08-02 14:33:38 +0500

    modified read me

--- src/module.mjs
+++
@@ -1,2 +0,0 @@
-import ui, {a, c, d} from './module1.mjs';
-console.log(ui, a, c, d);
--- src/module1.mjs
+++
@@ -1,9 +0,0 @@
-const a = 'araiz';
-const b = 'injir';
-const c = 'miswar';
-const d = 'pahto';
-
-export default b;
-export {a};
-export {c};
-export {d};
